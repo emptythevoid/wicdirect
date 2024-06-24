10234b815ad5c834c00655018e2ed8059ec8015c
emptythevoid <emptythevoid@gmail.com>
2024-06-24 10:53:13 -0400

    Add files via upload

---
+++ check_coreinterop.ps1
@@ -0,0 +1,9 @@
+﻿# Add this as a scheduled task to run as SYSTEM daily every few minutes.
+
+try {
+    Invoke-WebRequest -Method Head -Uri https://localhost:9999/Interop/
+}
+
+catch {
+    Restart-Service "BB.EBT.ServiceHosts.CoreInterop"
+}
